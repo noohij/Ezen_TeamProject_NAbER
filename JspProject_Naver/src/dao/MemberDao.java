ae2b4e6e0926e0db945cdfbf5ac70f48f1509824
Jihoon Park <jihoon.park0128@gmail.com>
2023-08-11 14:47:58 +0900

    2023-08-11 GitHub 충돌 오류 해결(MemberDao)

--- JspProject_Naver/src/dao/MemberDao.java
+++ JspProject_Naver/src/dao/MemberDao.java
@@ -4,12 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import dto.BoardDto;
-<<<<<<< HEAD
 import dto.MemberDto;
 
 public class MemberDao {
@@ -26,7 +22,7 @@ public class MemberDao {
 		PreparedStatement pstmt = null;
 
 		try {
-			
+
 			String id = memberDto.getId();
 			String pwd = memberDto.getPwd();
 			String email = memberDto.getEmail();
@@ -50,7 +46,7 @@ public class MemberDao {
 			pstmt.setString(2, pwd);
 			pstmt.setString(3, email);
 			pstmt.setString(4, name);
-			pstmt.setDate(5, (java.sql.Date)birthday);
+			pstmt.setDate(5, (java.sql.Date) birthday);
 			pstmt.setString(6, telecom);
 			pstmt.setString(7, gender);
 			pstmt.setString(8, nationality);
@@ -59,7 +55,7 @@ public class MemberDao {
 			resultNum = pstmt.executeUpdate();
 
 		} catch (Exception e) {
-			
+
 		} finally {
 			if (pstmt != null) {
 				try {
@@ -69,148 +65,65 @@ public class MemberDao {
 				}
 			}
 
-		} //end of finally
+		} // end of finally
 		return resultNum;
 	}
-	
-	public MemberDto memberExist(String id, String pwd) 
-			throws SQLException{
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
-			
-			String sql = "";
-			sql += "SELECT ID, PWD";
-			sql	+= " FROM MEMBERS";
-			sql += " WHERE ID = ?";
-			sql += "AND PWD = ?";
-			
-			String name = "";
-			
+
+	public MemberDto memberExist(String id, String pwd) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "";
+		sql += "SELECT ID, PWD";
+		sql += " FROM MEMBERS";
+		sql += " WHERE ID = ?";
+		sql += "AND PWD = ?";
+
+		String name = "";
+
+		try {
+			pstmt = connection.prepareStatement(sql);
+
+			int colIndex = 1;
+
+			pstmt.setString(colIndex++, id);
+			pstmt.setString(colIndex, pwd);
+
+			rs = pstmt.executeQuery();
+
+			MemberDto memberDto = new MemberDto();
+
+			if (rs.next()) {
+//				email = rs.getString("email");
+				name = rs.getString("mname");
+
+//				memberDto.setEmail(email);
+				memberDto.setName(name);
+
+				return memberDto;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
 			try {
-				pstmt = connection.prepareStatement(sql);
-				
-				int colIndex = 1;
-				
-				pstmt.setString(colIndex++, id);
-				pstmt.setString(colIndex, pwd);
-				
-				rs = pstmt.executeQuery();
-				
-				MemberDto memberDto = new MemberDto();
-
-				if (rs.next()) {
-//					email = rs.getString("email");
-					name = rs.getString("mname");
-					
-//					memberDto.setEmail(email);
-					memberDto.setName(name);
-					
-					return memberDto;
+				if (rs != null) {
+					rs.close();
 				}
-			} catch (Exception e) {
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}finally {
-				try {
-					if (rs != null) {
-						rs.close();
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			}
+			try {
+				if (pstmt != null) {
+					pstmt.close();
 				}
-				try {
-					if (pstmt != null) {
-						pstmt.close();
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} // finally 종료
-			
-			return null;
-		}
-}
-=======
-import dto.MemberDto;;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} // finally 종료
 
-
-
-public class MemberDao {
-	
-	private Connection connection;
-	
-	public void setConnection(Connection conn) {
-		this.connection = conn;
+		return null;
 	}
-	public MemberDto memberExist(String id, String pwd) 
-			throws SQLException{
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
-			
-			String sql = "";
-			sql += "SELECT ID, PWD";
-			sql	+= " FROM MEMBERS";
-			sql += " WHERE ID = ?";
-			sql += "AND PWD = ?";
-			
-			String name = "";
-			
-			try {
-				pstmt = connection.prepareStatement(sql);
-				
-				int colIndex = 1;
-				
-				pstmt.setString(colIndex++, id);
-				pstmt.setString(colIndex, pwd);
-				
-				rs = pstmt.executeQuery();
-				
-				MemberDto memberDto = new MemberDto();
 
-				if (rs.next()) {
-//					email = rs.getString("email");
-					name = rs.getString("mname");
-					
-//					memberDto.setEmail(email);
-					memberDto.setName(name);
-					
-					return memberDto;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}finally {
-				try {
-					if (rs != null) {
-						rs.close();
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				try {
-					if (pstmt != null) {
-						pstmt.close();
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} // finally 종료
-			
-			return null;
-		}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
-	
 }
->>>>>>> branch 'main' of https://github.com/imchanu96/Middle_TeamProject.git
